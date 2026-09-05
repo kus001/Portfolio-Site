2672b95670b6443a1b4767d2df3b7dcbae75fb4c
kus <166449142+kus001@users.noreply.github.com>
2026-09-05 18:07:59 -0400

    added proper proportions to projects and experiences pages

--- vite-project/src/sections/Experience.jsx
+++ vite-project/src/sections/Experience.jsx
@@ -46,7 +46,7 @@ export const Experience = () => {
         <section id="experience" className="py-32 relative overflow-hidden">
             <div className="absolute top-1/2 left-1/4 w-96 h-96 bg-primary/5 rounded-full blur-3xl -translate-y-1/2"/>
 
-            <div className="container mx-auto px-6 relative z-10">
+            <div className="container mx-auto px-6 relative z-10 -mt-21">
                 {/* section header */}
                 <div className="max-w-3xl mb-16">
                     <span className="text-secondary-foreground text-sm font-medium font-code tracking-widest uppercase animate-fade-in">// Journey //</span>
